84e3a71cbb973331c847500b1378c6b18c887d03
nourhamdan <nhamdan016@gmail.com>
2021-07-15 18:07:41 -0500

    query.sql

--- query.sql
+++ query.sql
@@ -1,1 +1,15 @@
-SELECT * FROM 
+-- spotify table
+CREATE TABLE spotify (
+	artists VARCHAR,
+	name  VARCHAR,
+	popularity INT,
+	explicit INT,
+	danceability INT,
+	loudness INT, 
+	speechiness INT, 
+	acousticness INT, 
+	instrumentalness INT,
+	tempo INT
+);
+
+SELECT * FROM spotify;
